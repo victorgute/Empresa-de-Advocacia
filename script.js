2795335416acde7d294155eab1cdccb9c5e0db48
victor <victor.h.gutierrez.carvalho>
2024-10-18 22:03:26 -0300

    primeiro commit

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
- // Ao clicar no botão Voltar ao Topo, rola a página para o topo
-document.querySelector('.back-to-top-button').addEventListener('click', function(e) {
-    e.preventDefault(); // Previne o comportamento padrão do link
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth' // Rola suavemente para o topo
+document.querySelectorAll('.btn-cta').forEach(button => {
+        button.addEventListener('click', () => {
+            window.open('https://wa.me/5511992593911', '_blank');
+        });
     });
-});
