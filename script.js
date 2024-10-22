e180dc9c9c1fce42b4eb41b29c6537916c60ee8b
victor <victor.h.gutierrez.carvalho>
2024-10-21 23:57:27 -0300

    quarto commit

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ document.querySelectorAll('.btn-cta').forEach(button => {
             window.open('https://wa.me/5511992593911', '_blank');
         });
     });
+
+
+
+// Pega o botão pelo ID
+const backToTopButton = document.getElementById('backToTop');
+
+// Função para mostrar ou esconder o botão dependendo da rolagem
+window.onscroll = function() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopButton.classList.add('show');
+    } else {
+        backToTopButton.classList.remove('show');
+    }
+};
+
+// Função para rolar suavemente ao topo
+backToTopButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Comportamento suave para subir ao topo
+    });
+});
