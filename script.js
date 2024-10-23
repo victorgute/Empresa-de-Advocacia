4af89b3f603ffaaebd25c25bb7ffd83567f6f5c9
victor <victor.h.gutierrez.carvalho>
2024-10-23 16:35:30 -0300

    decimo segundo commit

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-// Botão "Fale com Especialista"
 document.querySelectorAll('.cta-button').forEach(button => {
     button.addEventListener('click', () => {
         window.open('https://wa.me/5511992593911', '_blank');
     });
 });
 
-// Pega o botão "Voltar ao Topo" pelo ID
 const backToTopButton = document.getElementById('backToTop');
 
-// Função para mostrar ou esconder o botão dependendo da rolagem
 window.onscroll = function() {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         backToTopButton.classList.add('show');
@@ -17,11 +14,10 @@ window.onscroll = function() {
     }
 };
 
-// Função para rolar suavemente ao topo
 backToTopButton.addEventListener('click', function(e) {
     e.preventDefault();
     window.scrollTo({
         top: 0,
-        behavior: 'smooth' // Comportamento suave para subir ao topo
+        behavior: 'smooth'
     });
 });
